d9988c708cbf62247acd08a4ff33e1eb8f141b42
Jeremie Berduck <jeremieberduck@gmail.com>
2022-11-05 13:50:24 +0100

    Initializing with my apps

---
+++ Sources/ByMe/AppList.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jeremie Berduck on 05/11/2022.
+//
+
+import Foundation
+import SwiftUI
+
+final class MyApps {
+    // Create app objects
+    static let shoobie: AnApp = AnApp(name: "Shoobie", url: "https://apple.co/3tHjo8I", appicon: "shoobie")
+    static let demeter: AnApp = AnApp(name: "Demeter", url: "https://apple.co/3heu7VR", appicon: "demeter")
+    static let densha: AnApp = AnApp(name: "Densha Otaku", url: "https://apple.co/3E4Qsya", appicon: "densha")
+    static let yorikiri: AnApp = AnApp(name: "Yorikiri", url: "https://apple.co/3FHZSRo", appicon: "yorikiri-app")
+    
+    static let list: [AnApp] = [shoobie, demeter, densha, yorikiri]
+}
+
+@available(macOS 13.0, *)
+public struct MyAppsView: View {
+    public var body: some View {
+        ScrollView(.horizontal) {
+            HStack(alignment: .top, spacing: 8) {
+                ForEach(MyApps.list, id: \.self) { app in
+                    Link(destination: URL(string: app.url)!) {
+                        VStack(alignment: .center) {
+                            Image(app.appicon).frame(width: 44, height: 44).cornerRadius(12)
+                            Text(app.name).modifier(MyAppsNameModifier())
+                            Spacer()
+                        }.frame(height: 90).frame(maxWidth: 52).padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@available(macOS 13.0, *)
+struct MyAppsNameModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.caption, design: .serif, weight: .medium))
+            .multilineTextAlignment(.center)
+            .lineLimit(2)
+    }
+}
+
+public struct AnApp: Hashable {
+    var id = UUID().uuidString
+    var name, url, appicon: String
+}
--- Sources/ByMe/ByMe.swift
+++ Sources/ByMe/ByMe.swift
@@ -1,5 +1,6 @@
+@available(macOS 13.0, *)
 public struct ByMe {
-    public private(set) var text = "Hello, World!"
+    public private(set) var list = MyAppsView()
 
     public init() {
     }
